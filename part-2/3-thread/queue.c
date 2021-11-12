9c69b3421454e94d1e15ecfc696e9586b2ecad13
canplane <canplane@gmail.com>
2021-11-13 04:48:16 +0900

    211113-0448

--- part-2/3-thread/queue.c
+++ part-2/3-thread/queue.c
@@ -32,11 +32,11 @@ typedef struct {
 
 // set array to queue and return queue handler
 #define 		Queue__init(array)						__Queue__init((void *)(array), sizeof(array), sizeof(*array))
-Queue __Queue__init(void *array, int container_size, int element_size)
+Queue __Queue__init(void *array, int array_size, int element_size)
 {
 	Queue q;
 	q._array = array;
-	q._array_length = container_size / element_size;
+	q._array_length = array_size / element_size;
 	q._element_size = element_size;
 
 	q._front = q._back = 0;
@@ -48,8 +48,8 @@ Queue __Queue__init(void *array, int container_size, int element_size)
 #define			__Queue__addr(_q, _idx) 				(void *)((unsigned long)(_q)->_array + (_idx) * (_q)->_element_size)
 
 
-#define 		Queue__push(q, src)						__Queue__push((q), (void *)&(src))
-bool __Queue__push(Queue *q, const void *src)
+#define 		Queue__push(q, element)					__Queue__push((q), (void *)&(element))
+bool __Queue__push(Queue *q, const void *element)
 {
 	int tmp;
 	tmp = (q->_back + 1) % q->_array_length;
@@ -57,7 +57,7 @@ bool __Queue__push(Queue *q, const void *src)
 		return false;
 	q->_back = tmp;
 	q->_size++;
-	memcpy(__Queue__addr(q, q->_back), src, q->_element_size);
+	memcpy(__Queue__addr(q, q->_back), element, q->_element_size);
 	return true;
 }
 
